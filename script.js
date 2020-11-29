a48d6b2cb307c70d1dbc81ee0a63719008913655
Ana Paula Souza Dias <anapaula_sdias@hotmail.com>
2020-11-29 13:35:50 -0300

    alterações

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert('Olá');
+function verificar() {
+  var data = new Date();
+  var ano = data.getFullYear();
+  var formano = document.getElementById('txtano');
+  var res = document.querySelector('div#res');
+  var fsex = document.getElementsByTagName('radsex');
+  var idade = ano - Number(formano.value);
+  var genero = '';
+  var img = document.creatElement('img');
+  if (fsex[0].checked) {
+    genero = 'Homem';
+  } else if (fsex[1].checked) {
+    genero = 'Mulher';
+  }
+  res.style.textAlign = 'center';
+  res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+}
