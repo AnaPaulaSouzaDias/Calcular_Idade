c804c45beea8677c20d597725f0cb31aa25dd485
Ana Paula Souza Dias <anapaula_sdias@hotmail.com>
2020-12-07 19:58:55 -0300

    mais detalhes

--- script.js
+++ script.js
@@ -2,16 +2,39 @@ function verificar() {
   var data = new Date();
   var ano = data.getFullYear();
   var form = document.getElementById('txtano');
-  var res = document.querySelector('div#res');
-  var fsex = document.getElementsByTagName('radsex');
+  var res = document.getElementById('res');
+  var fsex = document.querySelectorAll("input[name='radsex']");
   var idade = ano - Number(form.value);
   var genero = '';
-  var img = document.creatElement('img');
+  var img = document.createElement('img');
+  img.setAttribute('id', 'foto');
   if (fsex[0].checked) {
     genero = 'Homem';
-  } else if (fsex[1].checked) {
+    if (idade >= 0 && idade < 10) {
+      img.setAttribute('src', 'foto-bebe-m.png');
+    } else if (idade >= 10 && idade < 20) {
+      img.setAttribute('src', 'foto-jovem-m.png');
+    } else if (idade >= 20 && idade < 50) {
+      img.setAttribute('src', 'foto-adulto-m.png');
+    } else {
+      mg.setAttribute('src', 'foto-idoso-m.png');
+    }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+    res.appendChild(img);
+  } else if (radsex[1].checked) {
     genero = 'Mulher';
+    if (idade >= 50) {
+      img.setAttribute('src', 'foto-idoso-f.png');
+    } else if (idade >= 18) {
+      img.setAttribute('src', 'foto-adulto-f.png');
+    } else if (idade >= 16) {
+      img.setAttribute('src', 'foto-jovem-f.png');
+    } else {
+      img.setAttribute('src', 'foto-bebe-f.png');
+    }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+    res.appendChild(img);
+  } else {
+    window.alert('[ERRO]-Sexo não especificado');
   }
-  res.style.textAlign = 'center';
-  res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
 }
