228a327d2bf8d63a2455932a98e535bece47ff10
Ana Paula Souza Dias <anapaula_sdias@hotmail.com>
2021-04-20 20:19:03 -0300

    finish

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 function verificar() {
-  var data = new Date()
-  var ano = data.getFullYear()
-  var form = document.getElementByName('txtano')
-  var res = document.getElementById('res')
-  var fsex = document.getElementsByName('radsex')
-  var idade = ano - Number(form.value)
-  var genero = ''
+  let data = new Date()
+  let ano = data.getFullYear()
+  let ano_nasc = document.getElementById('nasc_ano')
+  let res = document.getElementById('res')
+  let radius = document.getElementsByName('radsex')
+  let idade = ano - Number(ano_nasc.value)
+  let genero = ''
   var img = document.createElement('img')
-  img.setAttribute('id')
-  if (fsex[0].checked) {
-    genero = 'Homem'
+  img.setAttribute('id', 'foto')
+
+  if (radius[0].checked) {
+    genero = 'Masculino'
     if (idade >= 0 && idade < 10) {
       img.setAttribute('src', 'foto-bebe-m.png')
-    } else if (idade >= 10 && idade < 20) {
+    } else if (idade < 21) {
       img.setAttribute('src', 'foto-jovem-m.png')
-    } else if (idade >= 20 && idade < 50) {
+    } else if (idade < 59) {
       img.setAttribute('src', 'foto-adulto-m.png')
     } else {
       mg.setAttribute('src', 'foto-idoso-m.png')
     }
-    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-    res.appendChild(img)
-  } else if (fsex[1].checked) {
-    genero = 'Mulher';
-    if (idade >= 0 && idade <10) {
-      img.setAttribute('src', 'foto-bebe-f.png')    
-    } else if (idade < 21) {
-      img.setAttribute('src', 'foto-jovem-f.png')    
-    } else if (idade <50) {
-      img.setAttribute('src', 'foto-adulto-f.png')  
-    } else {
+
+  } else if (radius[1].checked) {
+    genero = 'Feminino'
+    if (idade >= 0 && idade < 10) {
       img.setAttribute('src', 'foto-bebe-f.png')
+    } else if (idade < 19) {
+      img.setAttribute('src', 'foto-jovem-f.png')
+    } else if (idade < 50) {
+      img.setAttribute('src', 'foto-adulto-f.png')
+    } else {
+      img.setAttribute('src', 'foto-idoso-f.png')
     }
-    res.style.textAlign = 'center'
-    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-    res.appendChild(img)
-  } 
+  }
+  res.style.textAlign = 'center'
+  res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+  res.appendChild(img)
 }
